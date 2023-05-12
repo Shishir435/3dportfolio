f120cd8bbb95124c92a9b6f8d89977f2f904e9fd
Shishir435 <shishirchaurasiya435@gmail.com>
2023-05-12 23:56:53 +0530

    Social icon

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,12 +8,12 @@ const Footer = () => {
     <div className='sticky w-full text-white flex gap-auto'>
       <div className='m-auto'>
         <div className='flex mb-3 gap-4 justify-center align-baseline'>
-          <a href="https://www.linkedin.com/in/shishir-chaurasiya/" target='_blank' rel="noopener noreferrer"><BsLinkedin /></a>
-          <a href="https://www.instagram.com/_shishir435/" target='_blank' rel="noopener noreferrer"><BsInstagram /></a>
-          <a href="https://github.com/Shishir435" target='_blank' rel="noopener noreferrer"><BsGithub /></a>
+          <a className=' hover:text-[#0056d2] hover:scale-125' href="https://www.linkedin.com/in/shishir-chaurasiya/" target='_blank' rel="noopener noreferrer"><BsLinkedin /></a>
+          <a className=' hover:text-[#f52887] hover:scale-125' href="https://www.instagram.com/_shishir435/" target='_blank' rel="noopener noreferrer"><BsInstagram /></a>
+          <a className=' hover:text-[#fff] hover:scale-125' href="https://github.com/Shishir435" target='_blank' rel="noopener noreferrer"><BsGithub /></a>
         </div>
         <p >
-          Copyright &copy; Shishir
+          Copyright&copy; Shishir
         </p>
       </div>
 
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,23 +12,23 @@ const Hero = () => {
 
       < div className={`${styles.heroHeadText} relative text-white  p-2`}>
         <div className='absolute heroContent text-left'>
-          <h1 >Hi, I'm <span className={`text-[${mainColor}]`}>Shishir</span></h1>
+          <h1 > <span className={`hover:text-[gray]`} >Hi, I'm </span> <span className={`text-[${mainColor}] hover:text-[gray]`}>Shishir</span></h1>
           <p className={`${styles.heroSubText} text-secondary`}>
             I develop user
             interfaces and web applications
           </p>
 
-          <div className={`header__socials text-[${mainColor}]`}>
-            <a href="https://www.linkedin.com/in/shishir-chaurasiya/" target='_blank' rel="noopener noreferrer"><BsLinkedin /></a>
-            <a href="https://www.instagram.com/_shishir435/" target='_blank' rel="noopener noreferrer"><BsInstagram /></a>
-            <a href="https://github.com/Shishir435" target='_blank' rel="noopener noreferrer"><BsGithub /></a>
+          <div className={`header__socials `}>
+            <a className={`text-[${mainColor}]`} href="https://www.linkedin.com/in/shishir-chaurasiya/" target='_blank' rel="noopener noreferrer"><BsLinkedin /></a>
+            <a className='instagram' href="https://www.instagram.com/_shishir435/" target='_blank' rel="noopener noreferrer"><BsInstagram /></a>
+            <a className='text-white' href="https://github.com/Shishir435" target='_blank' rel="noopener noreferrer"><BsGithub /></a>
           </div>
         </div>
       </div >
 
       <div style={{ zIndex: '15' }} className='absolute top-[82vh]   w-full flex justify-center cursor-pointer items-center '>
         <a href="#works">
-          <div className={`w-[35px] h-[64px] rotate-90 rounded-3xl border-4 border-[#0056d2] flex justify-center cursor-pointer items-start `}
+          <div className={`w-[35px] h-[64px] rotate-90 rounded-3xl border-4 border-[#0056d2] hover:scale-125 flex justify-center cursor-pointer items-start `}
           >
             <motion.div animate={{
               y: [0, 20, 0]
